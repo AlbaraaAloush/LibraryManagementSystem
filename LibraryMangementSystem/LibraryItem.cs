31e6959dd0f577a10e4cc80fe9e92960ad882622
Albaraa <albaraaaloush@gmail.com>
2025-12-14 06:48:55 +0300

    Add project files.

---
+++ LibraryMangementSystem/LibraryItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+namespace LibraryManagementSystem
+{
+    internal abstract class LibraryItem
+    {
+        // Static member (shared across all instances)
+        private static int nextId = 1;
+
+        // Backing field (protected by a property)
+        private string title;
+
+        // Properties 
+        public int Id { get; }
+
+        public string Title
+        {
+            // same as get {return title;}
+            get => title;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Title cannot be null or empty.");
+
+                title = value;
+            }
+        }
+
+        public bool IsAvailable { get; private set; }
+
+        protected LibraryItem(string title)
+        {
+            // Id iss assigned origirnal value, and then incremented
+            Id = nextId++;
+            Title = title;
+            IsAvailable = true;
+        }
+        public override string ToString()
+        {
+            return $"[{Id}] {Title} - Available: {IsAvailable}";
+        }
+    }
+}
